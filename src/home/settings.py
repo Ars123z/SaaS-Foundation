3269cd393f4007d9e48a4f9a599d4734ffc754b8
Ars123z <arsalannaziri0786@gmail.com>
2024-09-22 16:44:20 +0530

    added environmental vars

--- src/home/settings.py
+++ src/home/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&uh&gx3ta&&wzm_nh3-6e%@n!5h_6p#ee1y=u0hjwesx5lrdjq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG')) == "true"
 
 ALLOWED_HOSTS = [
     ".railway.app"
